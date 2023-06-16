f34157088045b55c8ac939181bc09117a32fc99e
Dhiraj kumar Nayak <dnayak696@gmail.com>
2023-06-16 22:13:21 +0530

    typescript react basic completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+let name : string;
+
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+// aliase are two type "type" and "interface"
+
+// In type 
+
+type X = {
+  a: string;
+  b: number;
+}
+
+
+type Y = X & {
+  c: string;
+  d: number;
+}
+
+// In Interface can be extended using the extends like class
+
+interface Guy extends Person {
+   profession: string;
+}
+
+type Person ={ 
+  name: string;
+  age: number;
+}
+
+let person : Person = {
+  name: "Piyush",
+  age: 22
+}
+
+//Array  of objects
+
+let lotsOfPeople: Person[];
+
+
+// function
+// "unknown" is defined instead of any
+
+/**
+ * never is used instead of "unknown"
+ */
+
+
+
+let printName : (name: string) => number;
+
+
+function printPerson(name: number){
+  console.log(name)
+}
+
+const  App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1> Hello world</h1>
     </div>
   );
 }
---
+++ src/components/InputField.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const InputField : React.FC = () =>{
+    return(
+        <form>
+             <input type="input" placeholder='Enter a task' className=''/>
+             <button> Go </button>
+        </form>
+    )
+}
+
+
+export default InputField;
